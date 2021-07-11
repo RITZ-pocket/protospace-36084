d134abc6e74441531b20c7e26b31ed6a85610a4c
RITZ-pocket <86193135+RITZ-pocket@users.noreply.github.com>
2021-07-11 19:01:58 +0900

    記事編集時のエラーを解消

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -33,7 +33,7 @@ class PrototypesController < ApplicationController
     @prototype = Prototype.find(params[:id])
     @prototype.update(prototype_params)
     if @prototype.save
-      render 'show'
+      redirect_to prototype_path
     else
       render 'edit'
     end
